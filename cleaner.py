8169f4acbc85a1194d797a96851919984d27f370
giveme5 <286674783@qq.com>
2019-04-28 20:53:50 +0800

    去重

---
+++ cleaner.py
@@ -0,0 +1,28 @@
+"""清洗员。用来清理有错的数据"""
+import csv
+from collections import defaultdict
+
+c_filename = "A古籍名和作者-full..csv"
+class Cleaner:
+  def __init__(self):
+    pass
+
+  def do_clean(self):
+    """去重，然后在打印为csv格式"""
+    #先获取name2count
+    name2count = defaultdict(int)
+    with open(c_filename) as f:
+      r = csv.reader(f)
+      for row in r:
+        bookName = row[0]
+        author = row[1]
+        # print(bookName,author)
+        key = f"{bookName},{author}"
+        name2count[key] +=1
+
+    for name,count in name2count.items():
+      print(name)
+
+if __name__ == '__main__':
+    c = Cleaner()
+    c.do_clean()
